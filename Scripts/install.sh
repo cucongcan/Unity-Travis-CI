a7d7ffbda99770585da959de5f684046502ddd1e
cucongcan <31461888+cucongcan@users.noreply.github.com>
2017-09-04 14:42:10 +0700

    Update install.sh

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -5,8 +5,8 @@
 echo 'Downloading from http://netstorage.unity3d.com/unity/472613c02cf7/MacEditorInstaller/Unity-2017.1.0f3.pkg: '
 curl -o Unity.pkg http://netstorage.unity3d.com/unity/472613c02cf7/MacEditorInstaller/Unity-2017.1.0f3.pkg
 
-# echo 'Installing Unity.pkg'
-# sudo installer -dumplog -package Unity.pkg -target /
+echo 'Installing Unity.pkg'
+sudo installer -dumplog -package Unity.pkg -target /
 
 # BASE_URL=http://netstorage.unity3d.com/unity
 # HASH=649f48bbbf0f
